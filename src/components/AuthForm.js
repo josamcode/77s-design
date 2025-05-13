d0995b1df037a59a3d4639b5921cd2c146004e94
josamcode <gergessamuel100@gmail.com>
2025-05-14 00:26:07 +0300

    Fix: useForm hook usage inside component

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,6 +1,24 @@
-const {
-  register,
-  handleSubmit,
-  formState: { errors },
-  watch,
-} = useForm();
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function AuthForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email")} placeholder="Email" />
+      {errors.email && <span>This field is required</span>}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
